1e332b244468d4387e7637a5bfa9dee7e7298a85
Elliot <ejjsharpe@gmail.com>
2021-02-02 16:10:33 +0000

    remove cors options

--- app.js
+++ app.js
@@ -4,14 +4,8 @@ const { handleCustomErrors, handlePSQLErrors, handle500, routeNotFound } = requi
 const app = express()
 const cors = require('cors')
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,
-    optionSuccessStatus: 200
-}
-
 app.use(express.json())
-app.use(cors(corsOptions))
+app.use(cors())
 
 
 
